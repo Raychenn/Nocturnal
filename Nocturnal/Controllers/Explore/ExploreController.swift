411c66fb5d9a766423f1801181fae9478ff00cd8
Raychenn <r0975929562@gmail.com>
2022-06-14 17:01:15 +0800

    Set up tab icons and add ignore firebase google service file rule

---
+++ Nocturnal/Controllers/Explore/ExploreController.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreController.swift
+//  Nocturnal
+//
+//  Created by Boray Chen on 2022/6/14.
+//
+
+import UIKit
+
+class ExploreController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
